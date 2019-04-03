beb0770dd1330433840ccf2795651e7b6360ca65
Murkoth, Jeevan <jeevan.murkoth@intel.com>
2019-04-03 16:59:45 -0700

    Asgn-I

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import './UserInput.css';
 
 class UserInput extends Component
 {
     render(props){
         return(
             <input type="text" 
+            className="userinput"
             onChange={this.props.change} 
             value={this.props.username}
             ></input>
